4b3e90c8263e17eb9f5d416e1feaa62574a6c1d6
unknown <utkarshsingh8920@gmail.com>
2026-06-25 19:55:31 +0530

    Area_of_traingle

--- Classwork/Area_Day2/area.py
+++ Classwork/Area_Day2/area.py
@@ -1,1 +1,13 @@
-"""Area"""
+"""-------Area of triangle----------"""
+base = float(input("Enter the base of the triangle: "))
+if base <= 0:
+    exit("Error: Base must be positive values.")
+height = float(input("Enter the height of the triangle: "))
+
+if  height <= 0:
+    exit("Error: Base and height must be positive values.")
+else:
+    print ("--------------------------------------------------")
+    print ("The base of triangle is = ",base)
+    print ("The hight of trangle is = ",height)
+    print("Area of Triangle =", 0.5 * base * height)
